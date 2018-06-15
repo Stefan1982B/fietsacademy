8b2a8fd5ad903566fd0474f1b92e42c52cacbccb
Stefan Schuddinck <stefanschuddinck@hotmail.com>
2018-06-15 11:20:03 +0200

    eerste fietsAcademy

---
+++ src/main/java/be/vdab/fietsacademy/entities/Docent.java
@@ -0,0 +1,52 @@
+package be.vdab.fietsacademy.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import be.vdab.fietsacademy.enums.Geslacht;
+
+@Entity
+@Table(name = "docenten")
+public class Docent implements Serializable{
+	private static final Long serialVersionUID = 1L;
+	
+	  @Id 
+	  private long id;  
+	  private String voornaam; 
+	  private String familienaam;
+	  private BigDecimal wedde; 
+	  private String emailAdres;
+	  @Enumerated(EnumType.STRING)
+	  private Geslacht geslacht;
+	  
+	public Geslacht getGeslacht() {
+		return geslacht;
+	}
+	public void setGeslacht(Geslacht geslacht) {
+		this.geslacht = geslacht;
+	}
+	public long getId() {
+		return id;
+	}
+	public String getVoornaam() {
+		return voornaam;
+	}
+	public String getFamilienaam() {
+		return familienaam;
+	}
+	public BigDecimal getWedde() {
+		return wedde;
+	}
+	public String getEmailAdres() {
+		return emailAdres;
+	}
+	
+	
+	
+}
---
+++ src/test/java/be/vdab/fietsacademy/repositories/DataSourceTest.java
@@ -0,0 +1,22 @@
+package be.vdab.fietsacademy.repositories;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class DataSourceTest {
+
+	@Test
+	public void goeGeïnitialiseer() {
+
+	}
+
+}
